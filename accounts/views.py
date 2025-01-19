52173df9911f6b93183be429f6daac202bedb7ed
Shijin1111 <151844395+Shijin1111@users.noreply.github.com>
2025-01-19 12:37:05 +0530

    index page added

--- accounts/views.py
+++ accounts/views.py
@@ -2,7 +2,9 @@ from django.shortcuts import render, redirect
 from django.contrib.auth import authenticate, login
 from django.contrib.auth.decorators import login_required
 
-# Host login view
+def home(request):
+    return render(request, 'home.html')
+
 def host_login(request):
     if request.method == 'POST':
         username = request.POST['username']
@@ -15,7 +17,6 @@ def host_login(request):
             return render(request, 'accounts/host_login.html', {'error': 'Invalid credentials or not a host.'})
     return render(request, 'accounts/host_login.html')
 
-# Competitor login view
 def competitor_login(request):
     if request.method == 'POST':
         username = request.POST['username']
@@ -35,3 +36,4 @@ def host_dashboard(request):
 @login_required
 def competitor_dashboard(request):
     return render(request, 'accounts/competitor_dashboard.html')
+
