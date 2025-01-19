579155f911370193367a5f2241545b4f9aa33f3b
Shijin1111 <151844395+Shijin1111@users.noreply.github.com>
2025-01-19 11:03:46 +0530

    user models added for host and competitors

--- accounts/models.py
+++ accounts/models.py
@@ -1,3 +1,9 @@
+from django.contrib.auth.models import AbstractUser
 from django.db import models
 
-# Create your models here.
+class CustomUser(AbstractUser):
+    USER_TYPES = [
+        ('host', 'Hackathon Host'),
+        ('competitor', 'Competitor'),
+    ]
+    user_type = models.CharField(max_length=10, choices=USER_TYPES, default='competitor')
