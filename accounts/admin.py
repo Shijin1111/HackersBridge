ed57d1214030778a2faaa4bfff26199f8c0054e2
Shijin1111 <151844395+Shijin1111@users.noreply.github.com>
2025-01-26 21:54:01 +0530

    host dashboard modified

--- accounts/admin.py
+++ accounts/admin.py
@@ -1,3 +1,30 @@
 from django.contrib import admin
+from django.contrib.auth.admin import UserAdmin
+from .models import CustomUser  # Import your custom user model
 
-# Register your models here.
+# Define a custom admin class for CustomUser
+class CustomUserAdmin(UserAdmin):
+    model = CustomUser
+    list_display = ['username', 'email', 'user_type', 'is_staff', 'is_active']
+    list_filter = ['user_type', 'is_staff', 'is_active']
+    search_fields = ['username', 'email']
+    ordering = ['username']
+
+    # Define the fields to be displayed on the user creation and change forms
+    fieldsets = (
+        (None, {'fields': ('username', 'password')}),
+        ('Personal info', {'fields': ('first_name', 'last_name', 'email')}),
+        ('Permissions', {'fields': ('is_active', 'is_staff', 'is_superuser', 'groups', 'user_permissions')}),
+        ('Important dates', {'fields': ('last_login', 'date_joined')}),
+        ('User Type', {'fields': ('user_type',)}),  # Make sure user_type is included here
+    )
+
+    add_fieldsets = (
+        (None, {'fields': ('username', 'password1', 'password2')}),
+        ('Personal info', {'fields': ('first_name', 'last_name', 'email')}),
+        ('Permissions', {'fields': ('is_active', 'is_staff', 'is_superuser', 'groups', 'user_permissions')}),
+        ('User Type', {'fields': ('user_type',)}),  # Include user_type when adding a user
+    )
+
+# Register the custom user model with the custom admin class
+admin.site.register(CustomUser, CustomUserAdmin)
