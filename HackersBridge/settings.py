9fb5ae6120c893c8acb9f47b42b018c524db3bc9
Shijin1111 <151844395+Shijin1111@users.noreply.github.com>
2025-06-02 10:27:17 +0530

    deployed using pythonanywhere and whitenoise.

--- HackersBridge/settings.py
+++ HackersBridge/settings.py
@@ -58,6 +58,7 @@ MIDDLEWARE = [
     'django.contrib.auth.middleware.AuthenticationMiddleware',
     'django.contrib.messages.middleware.MessageMiddleware',
     'django.middleware.clickjacking.XFrameOptionsMiddleware',
+    "whitenoise.middleware.WhiteNoiseMiddleware",
 ]
 
 ROOT_URLCONF = 'HackersBridge.urls'
@@ -146,5 +147,12 @@ ASGI_APPLICATION = 'HackersBridge.asgi.application'
 CHANNEL_LAYERS = {
     'default': {
         'BACKEND': 'channels.layers.InMemoryChannelLayer', 
+    },
+}
+
+STORAGES = {
+    # ...
+    "staticfiles": {
+        "BACKEND": "whitenoise.storage.CompressedManifestStaticFilesStorage",
     },
 }
